d8acb78e2a857266be5302d1ce0df6a634d6718f
Rizky Fajri <mansamussolini@gmail.com>
2022-05-14 15:53:53 +0700

    Menambahkan method di library

--- MatematikaLibraries/Class1.cs
+++ MatematikaLibraries/Class1.cs
@@ -2,6 +2,21 @@
 {
     public class Class1
     {
+        static void FPB(int input1, int input2)
+        {
 
+        }
+        static void KPK(int input1, int input2)
+        {
+
+        }
+        static void Turunan(int[] persamaan)
+        {
+
+        }
+        static void Integral(int[] persamaan)
+        {
+
+        }
     }
 }
